e948263fb0c3db12f158be4509bb54ff0db47a8b
Yan Bueno <buenoy519@gmail.com>
2025-06-26 16:18:30 -0300

    Alteração das configurações de conexão do Banco

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,13 +1,7 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
-    }
-)
+const Sequelize = require('sequelize');
+const connection = new Sequelize('comissao_yan', 'root', '@Yan2004', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 module.exports = connection;
