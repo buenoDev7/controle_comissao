e3247f85e5b250ee49492ace67b1e70b917179e0
Yan Bueno <buenoy519@gmail.com>
2025-06-27 13:43:58 -0300

    Adição de timeZone UTC para dataVendaFormatada

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,7 +1,13 @@
-const Sequelize = require('sequelize');
-const connection = new Sequelize('comissao_yan', 'root', '@Yan2004', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    }
+)
 
 module.exports = connection;
